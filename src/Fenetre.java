6a66c4638fbb106dd022e82b4a2ae5b8b8ef46d5
flotan11 <mardon.florian@gmail.com>
2017-02-28 13:38:48 +0100

    Re-début du menu
    Je recommit de suite car l'affichage n'etais pas celui que je voulais
    envoyer. Le bouton close fonctionne, il me reste a mieux afficher les
    éléments.

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -5,6 +5,8 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -19,23 +21,37 @@ public class Fenetre extends JFrame{
 		this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	    this.setResizable(false);
-	    this.setLayout();
 	    GridLayout gl=new GridLayout(4,1,100,100);
+	    
+	    JButton bout1=new JButton("GAME");
+	    JButton bout2=new JButton("Fast GAME");
+	    JButton bout3=new JButton("OPTION");
+	    JButton bout4=new JButton("EXIT");
+	    bout4.addActionListener(new ActionListener(){
+	    	  public void actionPerformed(ActionEvent event){
+	    		  exit();
+	    	  }
+	    });
 	    
 	    JPanel pan=new JPanel(gl);
 	    pan.setSize(800, 600);
-	    pan.add(new JButton("GAME"));
-	    pan.add(new JButton("Fast GAME"));
-	    pan.add(new JButton("OPTION"));
-	    pan.add(new JButton("EXIT"));
+	    pan.add(bout1);
+	    pan.add(bout2);
+	    pan.add(bout3);
+	    pan.add(bout4);
 	    JLabel lab=new JLabel(new ImageIcon("lib/background/menu.gif"));
 	    JPanel pan2=new JPanel();
 	    pan2.add(lab);
-	    this.add(pan2);
 	    this.add(pan);
+	    this.add(pan2);
 	    this.pack();
+	    this.repaint();
 	    
 		this.setVisible(true);
+	}
+	
+	public void exit(){
+		this.dispose();
 	}
 	
 }

7d0a019a45c4bd706ad8ffdaedfcfa6f609c6e52
flotan11 <mardon.florian@gmail.com>
2017-02-28 13:20:06 +0100

    Début du Menu
    Début du Menu (je finis dans l'aprem).

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,22 +1,39 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 
 public class Fenetre extends JFrame{
-	public Fenetre(){
+	public Fenetre() throws IOException{
 		this.setTitle("Unicorn ISLAND");
 		this.setSize(1200,800 );
 		this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	    this.setResizable(false);
+	    this.setLayout();
+	    GridLayout gl=new GridLayout(4,1,100,100);
 	    
-	    this.setLayout(new GridLayout(4, 1));
-	    this.getContentPane().add(new JButton("GAME"));
-	    this.getContentPane().add(new JButton("Fast GAME"));
-	    this.getContentPane().add(new JButton("OPTION"));
-	    this.getContentPane().add(new JButton("EXIT"));
+	    JPanel pan=new JPanel(gl);
+	    pan.setSize(800, 600);
+	    pan.add(new JButton("GAME"));
+	    pan.add(new JButton("Fast GAME"));
+	    pan.add(new JButton("OPTION"));
+	    pan.add(new JButton("EXIT"));
+	    JLabel lab=new JLabel(new ImageIcon("lib/background/menu.gif"));
+	    JPanel pan2=new JPanel();
+	    pan2.add(lab);
+	    this.add(pan2);
+	    this.add(pan);
+	    this.pack();
 	    
 		this.setVisible(true);
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		  Fenetre fen=new Fenetre();
 	}
 }

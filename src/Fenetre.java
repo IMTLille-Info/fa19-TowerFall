03918e5062398cede39bb199281b31f4dec628a7
flotan11 <mardon.florian@gmail.com>
2017-03-07 14:36:32 +0100

    Le menu tout beau finit !
    Plus qu'a faire les liens sur les autres partie et réordonner le code.

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -18,10 +18,9 @@ import javax.swing.JButton;
 
 
 public class Fenetre extends JFrame{
-	boolean music=false;
 	
 	public Fenetre() throws IOException{
-		Jukebox juke=new Jukebox();
+		Jukebox juke=new Jukebox(-1);
 		juke.start();
 		this.setTitle("Unicorn ISLAND");
 		this.setMinimumSize(new Dimension(1200,800));
@@ -49,18 +48,22 @@ public class Fenetre extends JFrame{
 	    pan.add(bout2);
 	    pan.add(bout3);
 	    pan.add(bout4);
-	    JLabel lab=new JLabel(new ImageIcon("lib/background/menu.gif"));
 	    JPanel pan2=new JPanel();
+	    JPanel pan3=new JPanel();
+	    JLabel lab=new JLabel(new ImageIcon("lib/background/menu.gif"));
+	    JLabel lab2=new JLabel(new ImageIcon("lib/background/unicorn-island.png"));
 	    pan2.setSize(1200, 800);
+	    pan3.setSize(800,400);
 	    pan2.add(lab);
-	    pan.setLocation(200,200);
+	    pan3.add(lab2);
+	    pan.setLocation(200,210);
+	    pan3.setLocation(200, 0);
+	    pan3.setOpaque(false);
+	    this.add(pan3);
 	    this.add(pan);
 	    this.add(pan2);
 	    this.pack();
 		this.setVisible(true);
-		if (music=false){
-			music=true;
-		}
 	}
 	
 	public void exit(){
--- src/Jukebox.java
+++ src/Jukebox.java
@@ -26,8 +26,10 @@ public class Jukebox extends Thread {
 	
 		List<String> musics=new ArrayList<String>();
 		Player player;
+		public static final int RAND = 10;
+		Integer choice=null;
 		
-	public Jukebox() {
+	public Jukebox(int i) {
 		// Menu :
 		musics.add("lib/music/Uncharted - Nate's Theme 0.5 (8-Bit Chiptune).mp3");
 		// Game-Music :
@@ -37,11 +39,27 @@ public class Jukebox extends Thread {
 		musics.add("lib/music/haddaway-what_is_love.mp3");
 		musics.add("lib/music/Los_del_Rio_-_Macarena.mp3");
 		musics.add("lib/music/Most awesome 8-bit song ever.mp3");
+		choice=i;
 	}
 
 	  public void run() {
+		  if (choice==(-1)){
 		    launchMenu();
+		  }else if(choice==RAND){
+			  randomLaunch();
+		  }else{
+			  launch(choice);
 		  }
+	  }
+	  
+	  public void run(int i) {
+		  if (i==RAND){
+		    randomLaunch();
+		  }else{
+			  launch(i);
+		  }
+	  }
+	  
 	public void launchMenu() {
 		try {
 			FileInputStream fis=new FileInputStream(musics.get(0));
@@ -57,12 +75,36 @@ public class Jukebox extends Thread {
 		}
 	}
 
-//	public void randomLaunch() {
-//		Random rand = new Random();
-//	}
+	public void randomLaunch() {
+		Random rand = new Random();
+		try {
+			FileInputStream fis=new FileInputStream(musics.get(rand.nextInt(musics.size())));
+			BufferedInputStream bis=new BufferedInputStream(fis);
+				player = new Player(bis);
+				player.play();
+		} catch (JavaLayerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-//	public void launch(int i) throws IOException, LineUnavailableException {
+	public void launch(int i) {
+		try {
+			FileInputStream fis=new FileInputStream(musics.get(i));
+			BufferedInputStream bis=new BufferedInputStream(fis);
+				player = new Player(bis);
+				player.play();
+		} catch (JavaLayerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-//	}
+	}
 
 }

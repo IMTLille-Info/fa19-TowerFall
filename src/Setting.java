3876a62bb4dc2000e7eda726bc90cbbadcd5e1ff
flotan11 <mardon.florian@gmail.com>
2017-03-08 19:50:22 +0100

    Fichier de setting pour les variables.
    Fichiers de settings destinés à recevoirs les différentes variables
    réglables du jeux (déplacement, options de jeux, son, ... ).

---
+++ src/Setting.java
@@ -0,0 +1,23 @@
+import java.awt.event.KeyEvent;
+
+public class Setting {
+	char J1_UP=KeyEvent.VK_UP;
+	char J1_DOWN=KeyEvent.VK_DOWN;
+	char J1_LEFT=KeyEvent.VK_LEFT;
+	char J1_RIGHT=KeyEvent.VK_RIGHT;
+	
+	char J2_UP='z';
+	char J2_DOWN='s';
+	char J2_LEFT='d';
+	char J2_RIGHT='q';
+	
+	char J3_UP='8';
+	char J3_DOWN='5';
+	char J3_LEFT='4';
+	char J3_RIGHT='6';
+	
+	char J4_UP='i';
+	char J4_DOWN='k';
+	char J4_LEFT='j';
+	char J4_RIGHT='l';
+}
